0123d8475b63ec0e3f6c78925e98326148d1c89c
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-12-20 23:53:29 +0900

    fix

--- src/hooks/funcs/syncBoard.ts
+++ src/hooks/funcs/syncBoard.ts
@@ -1,7 +1,6 @@
 import { useContext, Context, useSyncExternalStore } from "react";
 import type { Board, SetStore } from "../types";
 
-
 export function useStore<T>(Board: Context<Board<T>>, initialState: T): readonly [T, SetStore<T>];
 export function useStore<T, S>(Board: Context<Board<T>>, initialState: T, selector: (value: T) => S): readonly [S, SetStore<T>];
 export function useStore<T, S>(Board: Context<Board<T>>, initialState: T, selector?: (value: T) => S): any {
