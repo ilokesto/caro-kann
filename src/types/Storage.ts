344684d9d185613fcaba8adc4bc6e715f2dd9435
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-01-07 02:21:08 +0900

    fix: Storage 타입 정의에서 공백 정리

--- src/types/Storage.ts
+++ src/types/Storage.ts
@@ -1,6 +1,6 @@
 import { Migrate } from "./";
 
-type Storage = "local" | "session" | "cookie"  | null ;
+type Storage = "local" | "session" | "cookie" | null ;
 
 export type GetStorage = <T>(props: {storageKey: string, storageType: Storage, migrate?: Migrate<T>, initState: T}) => { state: T, version: number };
 export type SetStorage = <T>(props: {storageKey: string, storageVersion: number, value: T, storageType: Storage}) => void;
