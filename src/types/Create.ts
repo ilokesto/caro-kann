06dbdf7b6bc12af9f9d16a8073e58aaed4c737eb
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-01-12 22:20:50 +0900

    feat: Create 타입에서 OmitStoreContextFromReturnTypeCreate 제거 및 StoreContext 반환 추가

--- src/types/Create.ts
+++ src/types/Create.ts
@@ -1,24 +1,21 @@
 import { ReactNode } from "react";
 import { Dispatcher, Roll, Store, UseStore } from "./";
 
-type OmitStoreContextFromReturnTypeCreate<T, Z extends "basic" | "reducer" | "zustand" = "basic"> = Roll<Omit<ReturnTypeCreate<T, Z>, "StoreContext">>;
-
 export type ReturnTypeCreate<T, Z extends "basic" | "reducer" | "zustand" = "basic"> = {
   useStore: UseStore<T>[Z];
   useDerivedStore: <S>(selector: (state: T) => S) => S;
-  StoreContext: (props: { value: T, children: ReactNode }) => ReactNode;
 }
 
 export type Create = {
   // middleware persist & devtools
-  <T>(initState: [Store<T>, "persist" | "devtools"]): OmitStoreContextFromReturnTypeCreate<T>;
+  <T>(initState: [Store<T>, "persist" | "devtools"]): ReturnTypeCreate<T>;
 
   // middleware reducer
-  <T>(initState: [Store<T, Dispatcher>, "reducer"]): OmitStoreContextFromReturnTypeCreate<T, "reducer">;
+  <T>(initState: [Store<T, Dispatcher>, "reducer"]): ReturnTypeCreate<T, "reducer">;
 
   // middleware zustand
-  <T>(initState: [Store<T>, "zustand"]):OmitStoreContextFromReturnTypeCreate<T, "zustand">;
+  <T>(initState: [Store<T>, "zustand"]): ReturnTypeCreate<T, "zustand">;
 
   // create
-  <T>(initState: T): ReturnTypeCreate<T>;
+  <T>(initState: T): ReturnTypeCreate<T> & { StoreContext: (props: { value: T, children: ReactNode }) => ReactNode; };
 };
