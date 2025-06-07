16da01721521600796b7eb0ca84c0208a2b45c1a
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-06-07 16:08:19 +0900

    refactor: selected 변수를 사용하여 setSelected 및 storage 제거

--- dist/core/createStore.js
+++ dist/core/createStore.js
@@ -1,25 +1,23 @@
 export const createStore = (initState) => {
     const callbacks = new Set();
     let store = initState;
-    let storage = {};
+    let selected = {};
     const setStore = (nextState, actionName, selector) => {
         store = typeof nextState === "function"
             ? nextState(store)
             : nextState;
-        if (selector) {
-            setSelected(selector(store));
-        }
+        if (selector)
+            selected = selector(store);
         callbacks.forEach((cb) => cb());
     };
-    const setSelected = (value) => { storage = value; };
     return {
         setStore,
-        setSelected,
+        setSelected: (value) => { selected = value; },
+        getStore: (init) => init ? initState : store,
+        getSelected: () => selected,
         subscribe: (callback) => {
             callbacks.add(callback);
             return () => callbacks.delete(callback);
         },
-        getStore: (init) => init ? initState : store,
-        getSelected: () => storage,
     };
 };
--- src/core/createStore.ts
+++ src/core/createStore.ts
@@ -4,32 +4,26 @@ import type { Store } from "../types";
 export const createStore = <T>(initState: T): Store<T> => {
   const callbacks = new Set<() => void>();
   let store = initState;
-  
-  // Symbol 속성을 가진 객체 생성
-  let storage = {}
+  let selected = {};
 
   const setStore = (nextState: SetStateAction<T>, actionName?: string, selector?: (state: T) => any) => {
     store = typeof nextState === "function" 
       ? (nextState as (prev: T) => T)(store) 
       : nextState;
     
-    if (selector) {
-      setSelected(selector(store));
-    }
+    if (selector) selected = selector(store);
     
     callbacks.forEach((cb) => cb());
   };
-  
-  const setSelected = (value: any) => { storage = value };
 
   return {
     setStore,
-    setSelected,
+    setSelected: (value: any) => { selected = value },
+    getStore: (init?: 'init') => init ? initState : store,
+    getSelected: () => selected,
     subscribe: (callback: () => void) => {
       callbacks.add(callback);
       return () => callbacks.delete(callback);
     },
-    getStore: (init?: 'init') => init ? initState : store,
-    getSelected: () => storage,
   };
 };
