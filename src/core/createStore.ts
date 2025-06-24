98f7ee6d3b4d13a20385a52ba38e7403597c4218
ayden94 <wpfekdml@me.com>
2025-06-24 15:47:15 +0900

    fix

--- src/core/createStore.ts
+++ src/core/createStore.ts
@@ -1,5 +1,27 @@
 import type { Store, SetStateAction } from "../types";
 
+// export class CreateStore<T> implements Store<T> {
+//   private store: T;
+//   private callbacks: Set<() => void> = new Set();
+
+//   constructor(initState: T) { this.store = initState };
+
+//   getStore() { return this.store; }
+
+//   setStore(nextState: SetStateAction<T>) {
+//     this.store = typeof nextState === "function"
+//       ? (nextState as (prev: T) => T)(this.store)
+//       : nextState;
+
+//     this.callbacks.forEach((cb) => cb());
+//   }
+
+//   subscribe(callback: () => void) {
+//     this.callbacks.add(callback);
+//     return () => this.callbacks.delete(callback);
+//   }
+// }
+
 export function createStore<T>(initState: T): Store<T> {
   let store: T = initState;
   const callbacks: Set<() => void> = new Set();
